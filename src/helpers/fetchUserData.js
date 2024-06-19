03a6f4a9221e39cbfe74498a49949ac899e35e12
jd richards <jdrichardstech@gmail.com>
2024-06-19 15:46:14 -0400

    more comments

--- src/helpers/fetchUserData.js
+++ src/helpers/fetchUserData.js
@@ -3,10 +3,13 @@ import { doc, getDoc } from 'firebase/firestore'
 
 export const fetchUserData = () => {
   return new Promise((resolve, reject) => {
+    //tracks current auth state in Firebase
     auth.onAuthStateChanged(async (user) => {
       console.log('user', user.uid)
       if (user.uid) {
         console.log('fetchUserData', user)
+
+        // retrieve the information from the Firestore DB
         const docRef = doc(db, 'Users', user.uid)
         try {
           const docSnap = await getDoc(docRef)
@@ -15,6 +18,7 @@ export const fetchUserData = () => {
             resolve(docSnap.data())
           } else {
             reject(new Error('No user data found'))
+            return false
           }
         } catch (error) {
           reject(error)
