eabed579aae53eb5c2e90c790fe063b19d1725da
jd richards <jdrichardstech@gmail.com>
2024-06-19 16:06:19 -0400

    add comments

--- src/helpers/logout.js
+++ src/helpers/logout.js
@@ -1,5 +1,6 @@
 import { auth } from './firebase'
 
+// you can import and call this functionin any component. there is no need to fetch to the backend if you have a backend
 export const logout = async () => {
   try {
     //firebase logout
