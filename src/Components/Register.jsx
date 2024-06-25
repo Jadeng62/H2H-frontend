e89d1f6a965e81adbb281a86ccfa074868c07ee5
jdrichards-pursuit <jdrpursuit@gmail.com>
2024-06-25 11:53:03 -0400

    add comments

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,86 +1,88 @@
-import { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { toast } from 'react-toastify'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-} from 'firebase/auth'
+} from "firebase/auth";
 
-import { auth } from '../helpers/firebase'
-import { register } from '../helpers/register'
+import { auth } from "../helpers/firebase";
+import { register } from "../helpers/register";
 
 function Register() {
   const [newUser, setNewUser] = useState({
-    email: '',
-    password: '',
-    first_name: '',
-    last_name: '',
-  })
+    email: "",
+    password: "",
+    first_name: "",
+    last_name: "",
+  });
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
-    setNewUser({ ...newUser, [e.target.id]: e.target.value })
-  }
+    setNewUser({ ...newUser, [e.target.id]: e.target.value });
+  };
 
   const handleClearState = () => {
     setNewUser({
-      email: '',
-      first_name: '',
-      last_name: '',
-      photo: '',
-    })
-  }
+      email: "",
+      first_name: "",
+      last_name: "",
+      photo: "",
+    });
+  };
 
   const handleRegister = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     try {
-      const { email, password } = newUser
+      const { email, password } = newUser;
       // createUser in firebase
       const userCredential = await createUserWithEmailAndPassword(
         auth,
         email,
         password
-      )
+      );
 
       // you need the JWT token to authenticate protected routes on the backend
-      const token = await userCredential.user.getIdToken()
-      localStorage.setItem('token', token)
+      const token = await userCredential.user.getIdToken();
+      localStorage.setItem("token", token);
 
-      const { uid, photoURL } = auth.currentUser
+      const { uid, photoURL } = auth.currentUser;
 
       if (uid) {
-        const retrievedUser = await register(newUser, photoURL, uid)
+        //register first
+        const retrievedUser = await register(newUser, photoURL, uid);
+        // no sign in the new user with signInWithEmailAndPassword
         if (retrievedUser.uid) {
-          await signInWithEmailAndPassword(auth, email, password)
+          await signInWithEmailAndPassword(auth, email, password);
 
-          handleClearState()
-          toast.success('User Registered Successfully!!', {
-            position: 'top-center',
-          })
-          navigate('/profile')
+          handleClearState();
+          toast.success("User Registered Successfully!!", {
+            position: "top-center",
+          });
+          navigate("/profile");
         } else {
-          toast.error('User Not Found', {
-            position: 'top-center',
-          })
+          toast.error("User Not Found", {
+            position: "top-center",
+          });
         }
       }
     } catch (error) {
-      console.log(error.message)
+      console.log(error.message);
 
       toast.error(error.message, {
-        position: 'bottom-center',
-      })
+        position: "bottom-center",
+      });
     }
-  }
+  };
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div style={{ textAlign: "center" }}>
       <form onSubmit={handleRegister}>
         <h3>Sign Up</h3>
         <div>
           <label htmlFor="first_name">
-            First Name:{' '}
+            First Name:{" "}
             <input
               type="text"
               id="first_name"
@@ -93,7 +95,7 @@ function Register() {
           </label>
 
           <label htmlFor="last_name">
-            Last Name:{' '}
+            Last Name:{" "}
             <input
               type="text"
               id="last_name"
@@ -105,7 +107,7 @@ function Register() {
           </label>
 
           <label htmlFor="email">
-            Email Address:{' '}
+            Email Address:{" "}
             <input
               type="email"
               id="email"
@@ -118,7 +120,7 @@ function Register() {
           </label>
 
           <label htmlFor="password">
-            Password:{' '}
+            Password:{" "}
             <input
               type="password"
               placeholder="Enter password"
@@ -139,7 +141,7 @@ function Register() {
         </p>
       </form>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
