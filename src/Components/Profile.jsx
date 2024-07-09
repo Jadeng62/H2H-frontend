d919d2065acd07ec930b16b40f50281dedcdf440
Carlitos <carlitosdutan@pursuit.org>
2024-07-09 11:15:57 -0400

    monday work

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -98,6 +98,7 @@ import { logout } from "../helpers/logout";
 
 import placeholderImage from "../assets/placeholder.png";
 import { fakeUser } from "../helpers/fakeInfo";
+import UpcomingGames from "./UpcomingGames";
 
 function Profile() {
   const navigate = useNavigate();
@@ -133,44 +134,84 @@ function Profile() {
     getUser();
   }, []);
 
-  if (!userDetails) return alert(`Error with backend fetch data lost`);
+  // if (!userDetails) return alert(`Error with backend fetch data lost`);
 
   return (
-    <div
-      style={{ textAlign: "center" }}
-      className="profile-container text-text"
-    >
+    <div className="text-text h-screen">
       {fakeUser ? (
-        <>
-          <img
-            // src={fakeUser.photo || placeholderImage}
-            src={placeholderImage}
-            alt={fakeUser.first_name}
-            // className="profile-img"
-            style={{
-              marginTop: 100,
-              marginBottom: 20,
-              borderRadius: "50%",
-              width: 150,
-              height: 150,
-            }}
-          />
+        <div className="grid grid-cols-3 p-8">
+          <div className="col-span-1">
+            <div className="flex justify-center items-center bg-gray-500 w-3/4 rounded-lg">
+              <div className="min-w-fit p-4">
+                <div className="flex flex-row items-center justify-center mb-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="26"
+                    height="26"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-shield-half mr-2"
+                  >
+                    <path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z" />
+                    <path d="M12 22V2" />
+                  </svg>
+                  <p className="text-3xl font-bold">{`${fakeUser.first_name} ${fakeUser.last_name}`}</p>
+                </div>
 
-          <h1 className="profile-h1">{fakeUser.first_name}'s Profile Page</h1>
-
-          <p>Email: {fakeUser.email}</p>
-          <p>First Name: {fakeUser.first_name}</p>
-          <p>
-            Last Name: {fakeUser.last_name ? fakeUser.last_name : "Unknown"}
-          </p>
-
-          <button
-            onClick={handleLogout}
-            className="border-2 border-white p-2 rounded-md"
-          >
-            Logout
-          </button>
-        </>
+                <p className="font-bold mb-2">Win/Loss Record:</p>
+                <div className="flex">
+                  <div className="flex gap-1">
+                    <p>{fakeUser.user_wins}</p>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="lucide lucide-crown"
+                    >
+                      <path d="M11.562 3.266a.5.5 0 0 1 .876 0L15.39 8.87a1 1 0 0 0 1.516.294L21.183 5.5a.5.5 0 0 1 .798.519l-2.834 10.246a1 1 0 0 1-.956.734H5.81a1 1 0 0 1-.957-.734L2.02 6.02a.5.5 0 0 1 .798-.519l4.276 3.664a1 1 0 0 0 1.516-.294z" />
+                      <path d="M5 21h14" />
+                    </svg>
+                  </div>
+                  <div className="flex gap-1 mx-2">
+                    <p>{fakeUser.user_losses}</p>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="lucide lucide-circle-x"
+                    >
+                      <circle cx="12" cy="12" r="10" />
+                      <path d="m15 9-6 6" />
+                      <path d="m9 9 6 6" />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+              <div className="flex justify-center">
+                <img src={placeholderImage} className="w-1/3" alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="col-span-2">
+            <UpcomingGames />
+          </div>
+        </div>
       ) : (
         <>
           <h2>Loading...</h2>
---
+++ src/Components/UpcomingGames.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+const UpcomingGames = () => {
+  return (
+    <div className="border-2 border-white rounded-lg p-2">
+      <h1 className="text-3xl mb-4">Upcoming Games</h1>
+
+      <div className="relative overflow-x-auto rounded-lg">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-text uppercase bg-accent ">
+            <tr>
+              <th scope="col" className="px-6 py-4">
+                Date
+              </th>
+              <th scope="col" className="px-6 py-4">
+                Time
+              </th>
+              <th scope="col" className="px-6 py-4">
+                Match Up
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-white border-b">
+              <th
+                scope="row"
+                className="px-6 py-5 font-medium text-gray-900 whitespace-nowrap"
+              >
+                Apple MacBook Pro 17"
+              </th>
+              <td className="px-6 py-5">Silver</td>
+              <td className="px-6 py-5">Laptop</td>
+            </tr>
+            <tr className="bg-white border-b">
+              <th
+                scope="row"
+                className="px-6 py-5 font-medium text-gray-900 whitespace-nowrap"
+              >
+                Microsoft Surface Pro
+              </th>
+              <td className="px-6 py-5">White</td>
+              <td className="px-6 py-5">Laptop PC</td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800">
+              <th
+                scope="row"
+                className="px-6 py-5 font-medium text-gray-900 whitespace-nowrap"
+              >
+                Magic Mouse 2
+              </th>
+              <td className="px-6 py-5">Black</td>
+              <td className="px-6 py-5">Accessories</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default UpcomingGames;
