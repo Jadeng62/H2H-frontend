2403c1be55415284a681352f252c2c0b293d9b33
juligarc91 <julissagarcia@pursuit.org>
2024-07-11 13:56:58 -0400

    edited keys for form and made object for usestate

--- src/Components/MyTeamForm.jsx
+++ src/Components/MyTeamForm.jsx
@@ -1,106 +1,117 @@
 import { useState } from 'react';
 
 const MyTeamForm = () => {
-  const [teamName, setTeamName] = useState('');
-  const [teamPic, setTeamPic] = useState('');
-  const [logo, setLogo] = useState('');
-  const [player1Id, setPlayer1Id] = useState('');
-  const [player2Id, setPlayer2Id] = useState('');
-  const [player3Id, setPlayer3Id] = useState('');
-  const [player4Id, setPlayer4Id] = useState('');
-  const [player5Id, setPlayer5Id] = useState('');
-  const [captainId, setCaptainId] = useState('');
+    const [formData, setFormData] = useState({
+        teamName: '',
+        teamPic: '',
+        logo: '',
+        pointGuardId: 0,
+        shootingGuardId: 0,
+        smallForwardId: 0,
+        powerForwardId: 0,
+        centerId: 0,
+        captainId: 0,
+        teamWins: 0,
+        teamLoss: 0,
+        matchesPlayed: 0,
+    });
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const formData = {
-      team_name: teamName,
-      team_pic: teamPic,
-      logo: logo,
-      player1_id: player1Id,
-      player2_id: player2Id,
-      player3_id: player3Id,
-      player4_id: player4Id,
-      player5_id: player5Id,
-      captain_id: captainId,
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: name === 'pointGuardId' || name === 'shootingGuardId' || name === 'smallForwardId' || 
+                    name === 'powerForwardId' || name === 'centerId' || name === 'captainId' ||
+                    name === 'teamWins' || name === 'teamLoss' || name === 'matchesPlayed' ? parseInt(value, 10) : value
+        });
     };
-    console.log(formData);
-    
-    // Add fetch aqui!!
-    
-    // Clear form fields after submission
-    setTeamName('');
-    setTeamPic('');
-    setLogo('');
-    setPlayer1Id('');
-    setPlayer2Id('');
-    setPlayer3Id('');
-    setPlayer4Id('');
-    setPlayer5Id('');
-    setCaptainId('');
-  };
 
-  return (
-    <form onSubmit={handleSubmit} className='bg-white'>
-      <div>
-        <label>
-          Team Name:
-          <input type="text" value={teamName} onChange={(e) => setTeamName(e.target.value)} />
-        </label>
-      </div>
-      <div>
-        <label>
-          Team Picture URL:
-          <input type="text" value={teamPic} onChange={(e) => setTeamPic(e.target.value)} />
-        </label>
-      </div>
-      <div>
-        <label>
-          Logo URL:
-          <input type="text" value={logo} onChange={(e) => setLogo(e.target.value)} />
-        </label>
-      </div>
-      <div>
-        <label>
-          Player 1 ID:
-          <input type="text" value={player1Id} onChange={(e) => setPlayer1Id(e.target.value)} />
-        </label>
-      </div>
-      <div>
-        <label>
-          Player 2 ID:
-          <input type="text" value={player2Id} onChange={(e) => setPlayer2Id(e.target.value)} />
-        </label>
-      </div>
-      <div>
-        <label>
-          Player 3 ID:
-          <input type="text" value={player3Id} onChange={(e) => setPlayer3Id(e.target.value)} />
-        </label>
-      </div>
-      <div>
-        <label>
-          Player 4 ID:
-          <input type="text" value={player4Id} onChange={(e) => setPlayer4Id(e.target.value)} />
-        </label>
-      </div>
-      <div>
-        <label>
-          Player 5 ID:
-          <input type="text" value={player5Id} onChange={(e) => setPlayer5Id(e.target.value)} />
-        </label>
-      </div>
-      <div>
-        <label>
-          Captain ID:
-          <input type="text" value={captainId} onChange={(e) => setCaptainId(e.target.value)} />
-        </label>
-      </div>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
-  );
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //useeffect for POST fetch
+        console.log(formData);
+    };
+
+    const {
+        teamName,
+        teamPic,
+        logo,
+        pointGuardId,
+        shootingGuardId,
+        smallForwardId,
+        powerForwardId,
+        centerId,
+        captainId,
+        teamWins,
+        teamLoss,
+        matchesPlayed,
+    } = formData;
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Team Name:
+                <input type="text" name="teamName" value={teamName} onChange={handleChange} />
+            </label>
+            <br />
+            <label>
+                Team Picture URL:
+                <input type="text" name="teamPic" value={teamPic} onChange={handleChange} />
+            </label>
+            <br />
+            <label>
+                Logo URL:
+                <input type="text" name="logo" value={logo} onChange={handleChange} />
+            </label>
+            <br />
+            <label>
+                Point Guard ID:
+                <input type="number" name="pointGuardId" value={pointGuardId} onChange={handleChange} />
+            </label>
+            <br />
+            <label>
+                Shooting Guard ID:
+                <input type="number" name="shootingGuardId" value={shootingGuardId} onChange={handleChange} />
+            </label>
+            <br />
+            <label>
+                Small Forward ID:
+                <input type="number" name="smallForwardId" value={smallForwardId} onChange={handleChange} />
+            </label>
+            <br />
+            <label>
+                Power Forward ID:
+                <input type="number" name="powerForwardId" value={powerForwardId} onChange={handleChange} />
+            </label>
+            <br />
+            <label>
+                Center ID:
+                <input type="number" name="centerId" value={centerId} onChange={handleChange} />
+            </label>
+            <br />
+            <label>
+                Captain ID:
+                <input type="number" name="captainId" value={captainId} onChange={handleChange} />
+            </label>
+            <br />
+            <label>
+                Team Wins:
+                <input type="number" name="teamWins" value={teamWins} onChange={handleChange} />
+            </label>
+            <br />
+            <label>
+                Team Loss:
+                <input type="number" name="teamLoss" value={teamLoss} onChange={handleChange} />
+            </label>
+            <br />
+            <label>
+                Matches Played:
+                <input type="number" name="matchesPlayed" value={matchesPlayed} onChange={handleChange} />
+            </label>
+            <br />
+            <button type="submit">Submit</button>
+        </form>
+    );
 };
 
 export default MyTeamForm;
