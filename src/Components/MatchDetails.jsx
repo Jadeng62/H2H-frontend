20163a91f2b719a1004b5eff0cfc92e8ef34b5d2
Carlitos <carlitosdutan@pursuit.org>
2024-07-15 14:04:47 -0400

    formatted match details, need to highlight captain

--- src/Components/MatchDetails.jsx
+++ src/Components/MatchDetails.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { formattedDate, formattedTime } from "../helpers/helper";
 
 const MatchDetails = ({ upcomingGames, userTeam }) => {
   const [match, setMatch] = useState({});
   const { id } = useParams();
-  const [teamLineup, setTeamLineup] = useState([]);
-  const [opponentLineup, setOpponentLineup] = useState([]);
-  const [opponentTeam, setOpponentTeam] = useState({});
+  const [firstTeamDetails, setFirstTeamDetails] = useState(null);
+  const [firstTeamRoster, setFirstTeamRoster] = useState([]);
+  const [secondTeamDetails, setSecondTeamDetails] = useState(null);
+  const [secondTeamRoster, setSecondTeamRoster] = useState([]);
 
   const URL = import.meta.env.VITE_BASE_URL;
 
@@ -18,136 +20,181 @@ const MatchDetails = ({ upcomingGames, userTeam }) => {
   }, [upcomingGames, id]);
 
   useEffect(() => {
-    if (userTeam && match.id) {
-      fetch(`${URL}/api/teams/${userTeam.id}/users`)
-        .then((res) => res.json())
-        .then((data) => setTeamLineup(data))
-        .catch((error) => console.error("Error fetching team lineup:", error));
+    const fetchTeams = async () => {
+      if (match.id) {
+        try {
+          const firstTeamRes = await fetch(
+            `${URL}/api/teams/${match.team1_id}`
+          );
+          const firstTeamData = await firstTeamRes.json();
+          setFirstTeamDetails(firstTeamData);
 
-      const opponentTeamId =
-        userTeam.id === match.team1_id ? match.team2_id : match.team1_id;
-      fetch(`${URL}/api/teams/${opponentTeamId}/users`)
-        .then((res) => res.json())
-        .then((data) => setOpponentLineup(data))
-        .catch((error) =>
-          console.error("Error fetching opponent lineup:", error)
-        );
+          const secondTeamRes = await fetch(
+            `${URL}/api/teams/${match.team2_id}`
+          );
+          const secondTeamData = await secondTeamRes.json();
+          setSecondTeamDetails(secondTeamData);
+        } catch (error) {
+          console.error("Error fetching team details:", error);
+        }
+      }
+    };
+    fetchTeams();
+  }, [match.id]);
 
-      fetch(`${URL}/api/teams/${opponentTeamId}`)
-        .then((res) => res.json())
-        .then((data) => setOpponentTeam(data));
-    }
-  }, [userTeam, match.id]);
+  useEffect(() => {
+    const fetchRosters = async () => {
+      if (firstTeamDetails?.id && secondTeamDetails?.id) {
+        try {
+          const firstTeamRosterRes = await fetch(
+            `${URL}/api/teams/${firstTeamDetails.id}/users`
+          );
+          const firstTeamRosterData = await firstTeamRosterRes.json();
+          setFirstTeamRoster(firstTeamRosterData);
 
-  // useEffect(() => {
-  //   fetch(`${URL}/api/teams/`);
-  // });
+          const secondTeamRosterRes = await fetch(
+            `${URL}/api/teams/${secondTeamDetails.id}/users`
+          );
+          const secondTeamRosterData = await secondTeamRosterRes.json();
+          setSecondTeamRoster(secondTeamRosterData);
+        } catch (error) {
+          console.error("Error fetching team rosters:", error);
+        }
+      }
+    };
+    fetchRosters();
+  }, [firstTeamDetails, secondTeamDetails]);
 
   return (
-    <div className="text-text h-fit">
-      <div className="bg-secondary/30 text-white pb-2 pt-5 text-6xl text-center bebas-neue-regular">
-        <div className="flex justify-evenly">
-          <h1>{userTeam.team_name}</h1>
-          <h1>V.S. </h1>
-          <h1>{opponentTeam.team_name}</h1>
-        </div>
-      </div>
-      <div className="grid grid-cols-6 p-8">
-        <div className="col-span-2 w-fit">
-          <div className="border-2 flex-row flex-1 p-4 overflow-y-auto">
-            {match.id && (
-              <div>
-                <h1>{match.address}</h1>
-                <h1>{match.city}</h1>
-                <h1>{match.state}</h1>
-                <h1>{match.zip}</h1>
-                <h1>{match.start_datetime}</h1>
-              </div>
-            )}
-          </div>
-          <div className="border-2 flex-1 p-4 overflow-y-auto">
-            <table className="table-auto bg-background rounded-lg mx-10 mb-10 mt-5">
-              <thead className="text-left uppercase text-text">
-                <tr>
-                  <th className="pl-7 py-4">Player</th>
-                  <th className="pl-7 py-4">Position</th>
-                  <th className="px-6 py-4">
-                    Wins <span>/</span> <span>Losses</span>
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {opponentLineup &&
-                  opponentLineup.map((player) => {
-                    return (
-                      <tr
-                        key={player.id}
-                        className="bg-white border-b font-medium text-gray-600/60 hover:bg-gray-100"
-                      >
-                        <td className="px-6 py-5 text-black/80">
-                          {player.first_name} {player.last_name}
-                        </td>
-                        <td className="px-6 py-5">{player.position}</td>
-                        <td className="px-6 py-5">
-                          <div className="bg-background px-2 rounded inline-block">
-                            <span className="text-primary font-bold">
-                              {player.userWins}
-                            </span>{" "}
-                            <span className="font-bold text-text">/</span>{" "}
-                            <span className="text-accent font-bold">
-                              {player.userLosses}
-                            </span>
-                          </div>
-                        </td>
-                      </tr>
-                    );
-                  })}
-              </tbody>
-            </table>
-          </div>
-          <div className="border-2 flex-1 p-4 overflow-y-auto">
-            <table className="table-auto bg-background rounded-lg mx-10 mb-10 mt-5">
-              <thead className="text-left uppercase text-text">
-                <tr>
-                  <th className="pl-7 py-4">Player</th>
-                  <th className="pl-7 py-4">Position</th>
-                  <th className="px-6 py-4">
-                    Wins <span>/</span> <span>Losses</span>
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {teamLineup &&
-                  teamLineup.map((player) => {
-                    return (
-                      <tr
-                        key={player.id}
-                        className="bg-white border-b font-medium text-gray-600/60 hover:bg-gray-100"
-                      >
-                        <td className="px-6 py-5 text-black/80">
-                          {player.first_name} {player.last_name}
-                        </td>
-                        <td className="px-6 py-5">{player.position}</td>
-                        <td className="px-6 py-5">
-                          <div className="bg-background px-2 rounded inline-block">
-                            <span className="text-primary font-bold">
-                              {player.userWins}
-                            </span>{" "}
-                            <span className="font-bold text-text">/</span>{" "}
-                            <span className="text-accent font-bold">
-                              {player.userLosses}
-                            </span>
-                          </div>
-                        </td>
-                      </tr>
-                    );
-                  })}
-              </tbody>
-            </table>
-          </div>
-        </div>
+    <div className="text-text">
+      <div className="grid grid-cols-3 max-sm:grid-cols-1">
         <div>
-          <h1>Hello</h1>
+          <h1 className="bg-secondary/30 text-white pb-2 pt-5 text-6xl text-center bebas-neue-regular">
+            Team A
+          </h1>
+          <div className="flex justify-center">
+            <table
+              className="table-auto bg-background rounded-lg mx-10 w-full"
+              style={{ marginTop: "15%", marginBottom: "15%" }}
+            >
+              <thead className="text-left uppercase">
+                <tr>
+                  <th className="pl-7 py-4">Player</th>
+                  <th className="pl-7 py-4"> Position</th>
+                  {/* <th className="px-6 py-4">Wins/Losses</th> */}
+                </tr>
+              </thead>
+              <tbody>
+                {firstTeamRoster &&
+                  firstTeamRoster.map((player) => (
+                    <tr
+                      key={player.id}
+                      className="bg-white border-b font-medium text-gray-600/60 hover:bg-gray-100"
+                    >
+                      <td className="px-6 py-5 text-black/80">
+                        <div className="flex m-auto">
+                          <img src={player.photo} className="w-16" />
+                          <h1 className="my-auto">
+                            {player.first_name} {player.last_name}
+                          </h1>
+                        </div>
+                      </td>
+                      <td className="px-6 py-5">{player.position}</td>
+                      {/* <td className="px-6 py-5">{player.username}</td> */}
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="">
+          <h1 className="bg-secondary/30 text-white pb-2 pt-5 text-6xl text-center bebas-neue-regular">
+            Vs
+          </h1>
+          <div className="flex justify-center">
+            <div className="">
+              <table
+                className="table-auto bg-accent rounded-lg mx-10"
+                style={{ marginTop: "50%", marginBottom: "50%" }}
+              >
+                <thead className="text-left uppercase">
+                  <tr>
+                    <th className="pl-7 py-4">Match Details</th>
+                    {/* <th className="pl-7 py-4">Input</th> */}
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="bg-white border-b font-medium text-gray-600/60 hover:bg-gray-100">
+                    <td className="px-6 py-5 text-black">Location:</td>
+                    <td className="px-6 py-5">{`${match.address} ${match.city}, ${match.state} ${match.zip}`}</td>
+                  </tr>
+                  <tr className="bg-white border-b font-medium text-gray-600/60 hover:bg-gray-100">
+                    <td className="px-6 py-5 text-black">Date:</td>
+                    <td className="px-6 py-5">
+                      {formattedDate(match.start_datetime)}
+                    </td>
+                  </tr>
+                  <tr className="bg-white border-b font-medium text-gray-600/60 hover:bg-gray-100">
+                    <td className="px-6 py-5 text-black">Time:</td>
+                    <td className="px-6 py-5">
+                      {formattedTime(match.start_datetime)}
+                    </td>
+                  </tr>
+                  <tr className="bg-white border-b font-medium text-gray-600/60 hover:bg-gray-100">
+                    <td className="px-6 py-5 text-black">Winner</td>
+                    <td className="px-6 py-5">
+                      {match.completed ? match.match_winner : "TBD"}
+                    </td>
+                  </tr>
+                  <tr className="bg-white border-b font-medium text-gray-600/60 hover:bg-gray-100">
+                    <td className="px-5 py-5 text-black">
+                      Player of the Match
+                    </td>
+                    <td className="px-6 py-5">TBD</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div className="">
+          <h1 className="bg-secondary/30 text-white pb-2 pt-5 text-6xl text-center bebas-neue-regular">
+            Team B
+          </h1>
+          <div className="flex justify-center">
+            <table
+              className="table-auto bg-background rounded-lg mx-10 w-full"
+              style={{ marginTop: "15%", marginBottom: "15%" }}
+            >
+              <thead className="text-left uppercase">
+                <tr>
+                  <th className="pl-7 py-4">Player</th>
+                  <th className="pl-7 py-4"> Position</th>
+                  {/* <th className="px-6 py-4">Wins/Losses</th> */}
+                </tr>
+              </thead>
+              <tbody>
+                {secondTeamRoster &&
+                  secondTeamRoster.map((player) => (
+                    <tr
+                      key={player.id}
+                      className="bg-white border-b font-medium text-gray-600/60 hover:bg-gray-100"
+                    >
+                      <td className="px-6 py-5 text-black/80">
+                        <div className="flex m-auto">
+                          <img src={player.photo} className="w-16" />
+                          <h1 className="my-auto">
+                            {player.first_name} {player.last_name}
+                          </h1>
+                        </div>{" "}
+                      </td>
+                      <td className="px-6 py-5">{player.position}</td>
+                      {/* <td className="px-6 py-5">{player.username}</td> */}
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
