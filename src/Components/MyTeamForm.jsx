c3da16c5ddd5b44617b4ba4bcb42e657e31e42ce
juligarc91 <julissagarcia@pursuit.org>
2024-07-16 12:29:24 -0400

    added select menu

--- src/Components/MyTeamForm.jsx
+++ src/Components/MyTeamForm.jsx
@@ -5,7 +5,13 @@ const MyTeamForm = ({ userDetails }) => {
     const [formData, setFormData] = useState({
         team_name: '',
         team_pic: '',
-        logo: ''
+        logo: '',
+        captain_id: userDetails.id,
+        point_guard_id: 0,
+        shooting_guard_id: 0,
+        small_forward_id: 0,
+        power_forward_id: 0,
+        center_id: 0
     });
 
     const handleChange = (e) => {
@@ -18,10 +24,10 @@ const MyTeamForm = ({ userDetails }) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        if (!userDetails || !userDetails.user_team_id) {
-            console.error('User details or user team ID missing.');
-            return;
-        }
+        // if (!userDetails || !userDetails.user_team_id) {
+        //     console.error('User details or user team ID missing.');
+        //     return;
+        // }
 
         try {
             console.log('Creating team with form data:', formData);
@@ -32,18 +38,19 @@ const MyTeamForm = ({ userDetails }) => {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
+                    // add uid from user table  
                     team_name: formData.team_name,
                     team_pic: formData.team_pic,
                     logo: formData.logo,
                     captain_id: userDetails.id,
-                    point_guard_id: 0,
-                    shooting_guard_id: 0,
-                    small_forward_id: 0,
-                    power_forward_id: 0,
-                    center_id: 0,
-                    team_wins: 0,
-                    team_loss: 0,
-                    matches_played: 0
+                    point_guard_id: null,
+                    shooting_guard_id: null,
+                    small_forward_id: null,
+                    power_forward_id: null,
+                    center_id: null,
+                    team_wins: null,
+                    team_loss: null,
+                    matches_played: null
                 }),
             });
 
@@ -59,7 +66,17 @@ const MyTeamForm = ({ userDetails }) => {
         }
     };
 
-    const { team_name, team_pic, logo } = formData;
+    const { 
+        team_name, 
+        team_pic, 
+        logo,  
+        captain_id,
+        point_guard_id,
+        shooting_guard_id,
+        small_forward_id,
+        power_forward_id,
+        center_id
+    } = formData;
 
     return (
         <div className='team-form-container'>
@@ -98,6 +115,24 @@ const MyTeamForm = ({ userDetails }) => {
                         className='team-form-input'
                         onChange={handleChange} />
                 </label>
+                <select name="player" id="player" className="team-form-select">
+                    <option>-- Player Position --</option>
+                    <option value={point_guard_id} className="team-form-option">
+                            Point Guard
+                    </option>
+                    <option value={shooting_guard_id} className="team-form-option">
+                            Shooting Guard
+                    </option>
+                    <option value={small_forward_id} className="team-form-option">
+                            Small Forward
+                    </option>
+                    <option value={power_forward_id} className="team-form-option">
+                            Power Forward
+                    </option>
+                    <option value={center_id} className="team-form-option">
+                            Center
+                    </option>
+                </select>
                 <button type="submit" className='team-form-btn'>Create Team</button>
             </form>
         </div>
