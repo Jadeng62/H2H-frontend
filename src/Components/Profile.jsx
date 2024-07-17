9112cb2267ff88a3d2da5bf704f389b8d84ab9a1
Carlitos <carlitosdutan@pursuit.org>
2024-07-17 13:27:36 -0400

    fixed refreshing issue and upcoming games updates now

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -5,7 +5,24 @@ import placeholderImage from "../assets/placeholder.png";
 import UpcomingGames from "./UpcomingGames";
 import PlayerCard from "./PlayerCard";
 
-function Profile({ userDetails, userTeam, upcomingGames }) {
+function Profile() {
+  // const [userDetails, setUserDetails] = useState(null);
+
+  // useEffect(() => {
+  //   async function getUser() {
+  //     try {
+  //       const user = await getUserData();
+  //       if (user) {
+  //         setUserDetails(user);
+  //       }
+  //     } catch (error) {
+  //       console.error("Error fetching user data:", error);
+  //     }
+  //   }
+
+  //   getUser();
+  // }, []);
+
   return (
     <div className="text-text flex flex-col">
       <div className="bg-secondary/30 text-white pb-2 pt-5 text-6xl text-center bebas-neue-regular">
@@ -13,13 +30,10 @@ function Profile({ userDetails, userTeam, upcomingGames }) {
       </div>
       <div className="flex flex-col p-8 sm:flex-row flex-grow gap-7">
         <div className="flex justify-center w-full sm:w-1/3 sm:mb-0">
-          <PlayerCard userDetails={userDetails} userTeam={userTeam} />
+          <PlayerCard />
         </div>
         <div className="flex justify-center sm:w-2/3 flex-grow">
-          <UpcomingGames
-            userDetails={userDetails}
-            upcomingGames={upcomingGames}
-          />
+          <UpcomingGames />
         </div>
       </div>
     </div>
