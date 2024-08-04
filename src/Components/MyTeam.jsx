73ec2f2bf4848e39c5f532aa15b566736f3d71b0
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-08-04 18:23:20 -0400

    reverted My Team bg back to original

--- src/Components/MyTeam.jsx
+++ src/Components/MyTeam.jsx
@@ -271,14 +271,14 @@ const MyTeam = () => {
     <div className="min-h-screen">
       {/* {console.log(isUserTeamCaptain)} */}
       <div
-        style={{
-          backgroundImage: `url('https://res.cloudinary.com/dvmczcg3f/image/upload/t_crop/v1721837757/Head%202%20Head/basketball_courts_ezeu10.jpg')`,
-          // backgrounImage: `url('https://res.cloudinary.com/dvmczcg3f/image/upload/v1721839263/Head%202%20Head/Screenshot_2024-07-24_at_12.40.56_PM_tzqtdj.png')`,
-          backgroundSize: "auto", // Adjust the image size as needed
-          backgroundPosition: "center", // Center the image
-        }}
+      // style={{
+      //   backgroundImage: `url('https://res.cloudinary.com/dvmczcg3f/image/upload/t_crop/v1721837757/Head%202%20Head/basketball_courts_ezeu10.jpg')`,
+      //   // backgrounImage: `url('https://res.cloudinary.com/dvmczcg3f/image/upload/v1721839263/Head%202%20Head/Screenshot_2024-07-24_at_12.40.56_PM_tzqtdj.png')`,
+      //   backgroundSize: "auto", // Adjust the image size as needed
+      //   backgroundPosition: "center", // Center the image
+      // }}
       >
-        <h1 className="bg-background/70  text-white pb-2 pt-5  text-6xl text-center bebas-neue-regular">
+        <h1 className="bg-secondary/30  text-white pb-2 pt-5  text-6xl text-center bebas-neue-regular">
           My Team
         </h1>
       </div>
